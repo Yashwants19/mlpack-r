9360c7f13ea057c10b9d25896a4b4706f5572cf7
Yashwant <yashwantsingh.sngh@gmail.com>
2020-03-14 16:32:28 +0530

    Add Documentation

--- src/mlpack/bindings/octave/README.md
+++ src/mlpack/bindings/octave/README.md
@@ -1,4 +1,3 @@
-  
   Following commands can be use for test the octave bindings:
   (Assumption: Octave and All required dependices(libarmadillo, liboctave-dev) are installed.)
 
@@ -11,3 +10,8 @@
      $ octave-cli
      octave:1> source test_octave.m
      octave:2> test octave_binding_tests.m
+
+
+  In order to get documentation we can use:
+
+     octave:1> help test_octave.m
--- src/mlpack/bindings/octave/test_octave.m
+++ src/mlpack/bindings/octave/test_octave.m
@@ -1,3 +1,40 @@
+## -*- texinfo -*-
+## @deftypefn {Function File}{[@var{double_out}, @var{int_out}, @var{matrix_out}, @var{string_out}] =} test_octave(@var{matrix_in}, @var{double_in}, @var{int_in}, @var{string_in}, @var{flag1}, @var{flag2})
+##
+## A simple program to test Octave binding functionality.                             
+## 
+## @strong{Optional input}
+## @table @asis
+## @item @var{double_in} : 
+## Input double, must be 4.0.
+## @item @var{int_in} :
+## Input int, must be 12.
+## @item @var{string_in} :
+## Input string, must be "hello".
+## @item @var{flag1} :
+## Input flag, must be specified.  Default value `false`.
+## @item @var{flag2} :
+## Input flag, must not be specified.  Default value `false`.
+## @item @var{matrix_in} :
+## Input matrix.
+## @item @var{verbose} :
+## Display informational messages and the full list of parameters and timers at the end of execution.  Default value `false`.
+## @end table
+##
+## @strong{Return values}
+## @table @asis
+## @item @var{double_out} :
+## Output double, will be 5.0.
+## @item @var{int_out} :
+## Output int, will be 13.
+## @item @var{matrix_out} :
+## Output matrix.
+## @item @var{string_out} :
+## Output string, will be "hello2".
+## @end table
+## 
+## @end deftypefn
+
 function [double_out, int_out, matrix_out, string_out] = test_octave(matrix_in = zeros(1, 1), double_in, int_in, string_in, flag1 = false, flag2 = false)
 
   octfile1 = strcat(pwd,"/test_octave.oct");
